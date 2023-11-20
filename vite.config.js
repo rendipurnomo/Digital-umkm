0013535f2fef073a8bb351f5f1f683c47262fc2c
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-11-20 22:15:03 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  experimental: {
-    renderBuiltUrls(fileName, {  hostId,type }) {
-      if (type === 'public') {
-        return 'https://digital-umkm.vercel.app' + fileName
-      }
-      else if(path.extname(hostId) == '.js'){
-        return {runtime: `window.__assetsPath(${JSON.stringify(fileName)})`}
-      }
-      else{
-        return 'https://digital-umkm.vercel.app/assets/' + fileName
-      }
-    }
-  }
-})
+    experimental: {
+      renderBuiltUrl(
+        filename,
+        { hostType }
+      ) {
+        if (hostType === 'js') {
+          return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` };
+        } else {
+          return { relative: true };
+        }
+      },
+    },
+});
